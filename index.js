fe55d40af3d563f3e1385e4b789e0e037ca8d180
Jashem <mjashem@hotmail.com>
2018-12-31 13:57:35 +0600

    addedt frontend

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+})
 
 var todoRoutes = require('./routes/todos');
 
